31d132ce41bb92efabe9a7d05e368c6a18506a91
Alex Huot <huot.alex@gmail.com>
2020-12-18 11:13:37 -0500

    More changes
    Not sure what I'm doing right now.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * =================================================
+ * Description:
+ *  Test script.
+ * =================================================
+ * Project:   monkey-business
+ * Developer: Alex Huot - huot.alex@gmail.com
+ * Company:   Algorilla Inc.
+ * Github:    https://github.com/Algorilla
+ * 
+ * Copyright  Algorilla Inc. - 2020
+ * =================================================
+ */
+
+using System;
+
+namespace monkey_business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We'll surely avoid scurvy if we all eat an orange.");
+        }
+    }
+}
